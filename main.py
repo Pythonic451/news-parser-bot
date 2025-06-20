ccca8929ac6fe0e7b02c712771cded554b62f791
Pythonic451 <creativewriter56@yandex.ru>
2025-06-20 18:31:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from telegram_bot import send_to_telegram
 def main():
     keywords = ["Python", "ИИ", "GPT"]
     news = parse_habr_news(keywords)
-    
+
     TOKEN = "ВАШ_TELEGRAM_BOT_TOKEN"
     CHAT_ID = "ВАШ_CHAT_ID"
     
     for item in news:
-        message = f"{item['title']}\n{item['link']}"
+        message = f"{item['title']} \n https://habr.com{item['link']}"
         send_to_telegram(TOKEN, CHAT_ID, message)
 
 if __name__ == "__main__":
